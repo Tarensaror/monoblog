1186110c60b332e701e308bdcface8478cb64196
tarensaror <marc.ernst@aol.com>
2017-05-05 13:51:37 +0200

    meh

--- spring-web-demo/src/main/java/de/hska/lkit/demo/web/MonoblogController.java
+++ spring-web-demo/src/main/java/de/hska/lkit/demo/web/MonoblogController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 
 @Controller
 public class MonoblogController {
@@ -13,4 +15,14 @@ public class MonoblogController {
 	
 	return "monoblog";
 	}
+	
+	
+	 @MessageMapping("/hello")
+	 @SendTo("/topic/greetings")
+	 public Greeting greeting(HelloMessage message) throws Exception {
+	    Thread.sleep(1000); // simulated delay
+	    return new Greeting("Hello, " + message.getName() + "!");
+	 }
+	
+
 }
